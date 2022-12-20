7fac9a8209bfa2ffd0085459382120de7fdadb0d
Swati634 <120095594+Swati634@users.noreply.github.com>
2022-12-20 17:58:10 +0530

    Test 1,2 done and perfome Queue method
    Queue Methods
    - Push,pop,shift,unshift,slice,splice

--- script2.js
+++ script2.js
@@ -1,11 +1,12 @@
 //=======Test1 Divide Array length with unique numbers=======
 
 // const ar1 = [1, 1, 2, 3, 2, 1];
-const ar1 = [1, 1, 1, 1, 1];
+   const ar1 = [1, 3, 1, 2, 5, 2];
 // const ar1 = [2, 1, 2, 3, 3, 4, 5, 6, 5, 4];
 // const ar1 = [2, 1, 2, 3, 3, 4];
 
 let halfLength = ar1.length / 2;
+console.log("array length:", ar1.length);
 let p1 = [];
 let q1 = [];
 
@@ -20,7 +21,9 @@ for (let j = 0; j < ar1.length; j++) {
   }
   q1.push(ar1[j]);
 }
+console.log(q1, "q1");
 q1 = [...new Set(q1)];
+console.log(q1, "new")
 
 console.log(p1, "p1");
 console.log(q1, "q1");
@@ -39,7 +42,7 @@ let x = [
 ];
 let y = [
   [1, 0, 0],
-  [0, 1, 0],
+  [0, 1, 0], 
   [1, 0, 1],
 ];
 let z = [
@@ -60,7 +63,7 @@ let bl = [
 ];
 
 function IMatrix(matrix_num) {
-  console.log(matrix_num.length, "Return length of matrix_num");
+  //console.log(matrix_num.length, "Return length of matrix_num");
   for (var i = 0; i < matrix_num.length; i++) {
     rows = matrix_num.length;
     cols = matrix_num[i].length;
@@ -90,50 +93,101 @@ console.log(IMatrix(bl));
 
 //=====Test 3 Javacript implement queue  =========
 
-function myQueue() {
-  this.elements = [];
-}
-console.log(myQueue.prototype, "myqueue");
-myQueue.prototype.enqueue = function (record) {
-  this.elements.push(record);
-};
-myQueue.prototype.dequeue = function () {
-  this.elements.shift();
-};
-myQueue.prototype.isEmpty = function () {
-  return this.elements.length == 0;
-};
-myQueue.prototype.peek = function () {
-  return this.isEmpty() ? undefined : this.elements[0];
-};
-myQueue.prototype.len = function () {
-  return this.elements.length;
-};
-let il = new myQueue();
-il.enqueue(1);
-il.enqueue(2);
-il.enqueue(3);
-il.enqueue(4);
-document.getElementById("demo").innerHTML = JSON.stringify(il);
-console.log(JSON.stringify(il));
+// function myQueue() {
+//   this.elements = [];
+// }
+// console.log(myQueue.prototype, "myqueue");
+// myQueue.prototype.enqueue = function (record) {
+//   this.elements.push(record);
+// };
+// myQueue.prototype.dequeue = function () {
+//   this.elements.shift();
+// };
+// myQueue.prototype.isEmpty = function () {
+//   return this.elements.length == 0;
+// };
+// myQueue.prototype.peek = function () {
+//   //return this.isEmpty() ? undefined : this.elements[0];
+//   return this.elements[0];
+// };
+// myQueue.prototype.len = function () {
+//   return this.elements.length;
+// };
+// let il = new myQueue();
+// il.enqueue(1);
+// il.enqueue(2);
+// il.enqueue(3);
+// il.enqueue(4);
+// document.getElementById("demo").innerHTML = JSON.stringify(il);
+// console.log(JSON.stringify(il));
 
-document.getElementById("demo1").innerHTML = il.peek();
-console.log("Peek that is return first value:", il.peek());
-document.getElementById("demo2").innerHTML = il.len();
-console.log("length that is return total length of the elements:", il.len());
-document.getElementById("demo3").innerHTML = il.isEmpty();
-console.log("isEmpty that is return in queue empty or not:", il.isEmpty());
-il.dequeue();
-document.getElementById("demo4").innerHTML = JSON.stringify(il);
-console.log(JSON.stringify(il), "il");
-document.getElementById("demo1").innerHTML = il.peek();
-console.log("Peek that is return first value:", il.peek());
-document.getElementById("demo2").innerHTML = il.len();
-console.log("length that is return total length of the elements:", il.len());
-il.dequeue();
-il.dequeue();
-il.dequeue();
-document.getElementById("demo4").innerHTML = JSON.stringify(il);
-console.log(JSON.stringify(il), "il");
-document.getElementById("demo3").innerHTML = il.isEmpty();
-console.log("isEmpty that is return in queue empty or not:", il.isEmpty());
+// document.getElementById("demo1").innerHTML = il.peek();
+// console.log("Peek that is return first value:", il.peek());
+// document.getElementById("demo2").innerHTML = il.len();
+// console.log("length that is return total length of the elements:", il.len());
+// document.getElementById("demo3").innerHTML = il.isEmpty();
+// console.log("isEmpty that is return in queue empty or not:", il.isEmpty());
+// il.dequeue();
+// document.getElementById("demo4").innerHTML = JSON.stringify(il);
+// console.log(JSON.stringify(il), "il");
+// document.getElementById("demo1").innerHTML = il.peek();
+// console.log("Peek that is return first value:", il.peek());
+// document.getElementById("demo2").innerHTML = il.len();
+// console.log("length that is return total length of the elements:", il.len());
+// il.dequeue();
+// il.dequeue();
+// il.dequeue();
+// document.getElementById("demo4").innerHTML = JSON.stringify(il);
+// console.log(JSON.stringify(il), "il");
+// document.getElementById("demo3").innerHTML = il.isEmpty();
+// console.log("isEmpty that is return in queue empty or not:", il.isEmpty());
+
+
+//======== Queue Methods=====
+
+let queue = [];
+queue.push(11);
+queue.push(12);
+queue.push(13);
+queue.push(14);
+queue.push(15);
+queue.push(14);
+
+console.log("The queue value is:", queue);
+let del = queue.shift();
+console.log("Remove element is:", del)
+console.log("The queue value is:", queue);
+console.log(queue.length,"length");
+if(queue.length > 0)
+{
+  console.log("Queue is not empty");
+}
+else{
+  console.log("Queue is empty");
+}
+let li = queue.lastIndexOf(14);
+console.log("last index value of 14 in  queue:", li);
+console.log("The queue value is:", queue);
+
+let del_l = queue.pop();
+console.log("Remove last element that is contains 4th index position from the queue:", del_l)
+console.log("The queue value is:", queue);
+
+let us = queue.unshift(11);
+console.log("11 is add in queue at 1st position:");
+console.log("The queue value is:", queue);
+
+let sc = queue.slice(1,4);
+console.log(sc,"Slice the value");
+console.log("The queue value is:", queue);
+
+let sp = queue.splice(3, 0, 10, 20);
+console.log("Add the 3rd position element",sp);
+console.log("The queue value is:", queue);
+
+sp = queue.splice(3,1);
+//sp = queue.splice(3,2);  If you want to removed 3rd and 4th position value
+console.log("Removed 3rd position element", sp);
+console.log("The queue value is:", queue);
+
+
